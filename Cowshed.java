ab0ef533f5107008c208a84102a0b49703f557ae
Michał Guziński <64793134+Czoso@users.noreply.github.com>
2020-07-11 22:49:55 +0200

    Add files via upload

--- Cowshed.java
+++ Cowshed.java
@@ -4,10 +4,14 @@ public class Cowshed {
 
     private int size;
     private String name;
+    private int status;
+    int price;
 
-    public Cowshed (int size, String name){
+    public Cowshed (int size, String name, int status, int price){
         this.size=size;
         this.name=name;
+        this.status=status;
+        this.price=price;
     }
     public void setSize(int size){
         this.size=size;
@@ -17,5 +21,17 @@ public class Cowshed {
     }
     public int getSize(){
         return size;
+    }
+    public void setStatus(int status){
+        this.status=status;
+    }
+    public int getStatus(){
+        return status;
+    }
+    public int getPrice(){
+        return status;
+    }
+    public void setPrice(int status){
+        this.status=status;
     }
 }
